418d99b6ee5aa9edbb60bb06f9f2bcf635590dfa
Adrian Del Grosso <10929341+ad3154@users.noreply.github.com>
2024-09-26 17:33:26 -0600

    Update license info, updates to work with latest CAN stack
    Clarify license in installer.
    Fixed a bug where working set icon background would be incorrectly drawn as all red.
    Fix call to internal control function factory which has since had its api updated.

--- src/WorkingSetComponent.cpp
+++ src/WorkingSetComponent.cpp
@@ -33,7 +33,7 @@ WorkingSetComponent::WorkingSetComponent(std::shared_ptr<isobus::VirtualTerminal
 void WorkingSetComponent::paint(Graphics &g)
 {
 	auto vtColour = parentWorkingSet->get_colour(get_background_color());
-	auto background = Colour(vtColour.r, vtColour.g, vtColour.b, 1.0f);
+	auto background = Colour::fromFloatRGBA(vtColour.r, vtColour.g, vtColour.b, 1.0f);
 	g.setColour(background);
 	g.fillAll();
 }
