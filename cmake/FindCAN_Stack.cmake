654b13a13800c6857ff0fe95d84b421cd41e9115
Adrian Del Grosso <10929341+ad3154@users.noreply.github.com>
2024-10-13 11:46:52 -0600

    Update CMake reference to CAN Stack

--- cmake/FindCAN_Stack.cmake
+++ cmake/FindCAN_Stack.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
   CAN_Stack
   GIT_REPOSITORY https://github.com/Open-Agriculture/AgIsoStack-plus-plus.git
-  GIT_TAG vt-server-managed-working-set
+  GIT_TAG 29dab887a48bb204aae983b06052d52b0f2314d5
 )
 FetchContent_MakeAvailable(CAN_Stack)
 endif()
