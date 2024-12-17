2f0905ba7fe93a0f3792f902a5ea2927591e3db6
Miklos Marton <martonmiklosqdev@gmail.com>
2024-12-18 00:15:33 +0100

    Support setting multiple softkey columns
    Fixes #18

--- cmake/FindCAN_Stack.cmake
+++ cmake/FindCAN_Stack.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
   CAN_Stack
   GIT_REPOSITORY https://github.com/Open-Agriculture/AgIsoStack-plus-plus.git
-  GIT_TAG 67e231298f2ca8367d437d903e396aa3bcefe080
+  GIT_TAG c9ffdc2d92ede8634c2d7ed128c59a08f2847245
 )
 FetchContent_MakeAvailable(CAN_Stack)
 endif()
