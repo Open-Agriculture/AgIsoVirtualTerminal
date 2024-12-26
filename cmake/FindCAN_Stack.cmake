ba1c6caeaebb6b2611f1e3ba837cdaf23399074c
GwnDaan <d.steenbergen@student.tue.nl>
2024-12-26 11:14:11 +0100

    chore: update git_tag after PR merge

--- cmake/FindCAN_Stack.cmake
+++ cmake/FindCAN_Stack.cmake
@@ -3,6 +3,6 @@ if(NOT TARGET isobus::isobus)
   FetchContent_Declare(
     CAN_Stack
     GIT_REPOSITORY https://github.com/Open-Agriculture/AgIsoStack-plus-plus.git
-    GIT_TAG db00fa406a6d83bf8cb75cc32650eb29e0f396e9)
+    GIT_TAG 68bc01550156a0d9ce7f191d2be8145cdc084771)
   FetchContent_MakeAvailable(CAN_Stack)
 endif()
