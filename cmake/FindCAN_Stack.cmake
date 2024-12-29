63bb2aac7e7eca28d0d7cc81737d277adbc1d4ed
Miklos Marton <martonmiklosqdev@gmail.com>
2024-12-29 10:37:17 +0100

    Point to the proper AgIsoStak-plusplus

--- cmake/FindCAN_Stack.cmake
+++ cmake/FindCAN_Stack.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
   CAN_Stack
   GIT_REPOSITORY https://github.com/Open-Agriculture/AgIsoStack-plus-plus.git
-  GIT_TAG c9ffdc2d92ede8634c2d7ed128c59a08f2847245
+  GIT_TAG d6dfd16f35f8eb401af5bab65c2ca6284a6761d3
 )
 FetchContent_MakeAvailable(CAN_Stack)
 endif()
