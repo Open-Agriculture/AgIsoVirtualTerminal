45fb25630fe88373143e351c26f617d36dbc8aea
Miklos Marton <martonmiklosqdev@gmail.com>
2024-12-29 10:37:17 +0100

    Point to the proper AgIsoStak-plusplus

--- cmake/FindCAN_Stack.cmake
+++ cmake/FindCAN_Stack.cmake
@@ -5,4 +5,3 @@ if(NOT TARGET isobus::isobus)
     GIT_REPOSITORY https://github.com/Open-Agriculture/AgIsoStack-plus-plus.git
     GIT_TAG 4ad80207b5e44d041d8e0fba8ef044367dfbfe86)
   FetchContent_MakeAvailable(CAN_Stack)
-endif()
