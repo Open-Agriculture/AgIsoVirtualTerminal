0869637cc33ae4c2aa914d9e2b6d72d319b88ce3
Miklos Marton <martonmiklosqdev@gmail.com>
2025-01-01 00:32:56 +0100

    Add IOP load indication
    Fixes #4

--- cmake/FindCAN_Stack.cmake
+++ cmake/FindCAN_Stack.cmake
@@ -3,6 +3,6 @@ if(NOT TARGET isobus::isobus)
   FetchContent_Declare(
     CAN_Stack
     GIT_REPOSITORY https://github.com/Open-Agriculture/AgIsoStack-plus-plus.git
-    GIT_TAG 5e1f9f600cc626463d92a3278707b60047b4a2cd)
+    GIT_TAG 30e51211a760c2df064dc2934a4f180c4cad73cc)
   FetchContent_MakeAvailable(CAN_Stack)
 endif()
