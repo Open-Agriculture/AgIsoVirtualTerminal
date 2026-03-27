e48afd18a2825cf895a67926f55ec7ce9b60b673
Miklos Marton <martonmiklosqdev@gmail.com>
2026-03-27 08:25:12 +0100

    Fix warning about int to bool comparison (reported during MSVC compilation)

--- src/ButtonComponent.cpp
+++ src/ButtonComponent.cpp
@@ -64,7 +64,7 @@ void ButtonComponent::paintButton(Graphics &, bool, bool)
 void ButtonComponent::set_options(std::uint8_t value)
 {
 	// adjust the position of the childs to the button face area if the NoBorder attribute is changed
-	if ((value & static_cast<std::uint8_t>(Options::NoBorder)) != get_option(Options::NoBorder))
+	if (((value & static_cast<std::uint8_t>(Options::NoBorder)) != 0) != get_option(Options::NoBorder))
 	{
 		auto borderOffset = (0 != (value & static_cast<std::uint8_t>(Options::NoBorder))) ? -4 : 4;
 		int i = 0;
